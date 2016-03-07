9f0c04255a54ad3cfa17680374025b980bb074c8
ljh_2015 <469874564@qq.com>
2016-03-07 20:44:22 +0800

    tomcat:run tomcat:deploy

--- wiki/src/main/java/cn/sdu/wiki/bean/getBeans.java
+++ wiki/src/main/java/cn/sdu/wiki/bean/getBeans.java
@@ -1,11 +1,8 @@
 package cn.sdu.wiki.bean;
 
-//import org.springframework.beans.factory.BeanFactory;
-//import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-//import org.springframework.core.io.ClassPathResource;
-//import org.springframework.core.io.Resource;
+
 
 public class getBeans {
 	
